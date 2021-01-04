30a64ebc6d28e74ae510f39e4ab2e022ab599881
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-01-04 13:30:17 +0100

    Finished project Birthday Reminder

--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({people, setPeople}) => {
   return (
     <>
-      <h2>list component</h2>
+      <div className="container">
+        <h3>{people.length} birthdays today</h3>
+        <ul>
+          {people.map((person) => {
+            return (
+              <li key={person.id} className="person">
+                <img src={person.image}></img>
+                <div>
+                  <h4>{person.name}</h4>
+                  <p>{person.age}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <button type="button" className="btn" onClick={() => {setPeople([])} }>Clear All</button>
+      </div>
     </>
   );
 };
