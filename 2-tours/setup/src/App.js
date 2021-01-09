3a91eda0e7227a48f42d1105a0af878c6e102a48
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-01-09 15:39:59 +0100

    finished tours project

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -1,11 +1,80 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [tours, setTours] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const fetchTours = () => {
+    fetch(url)
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          return response.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((response) => {
+        setTours(response);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setIsError(true);
+        console.log(error.msg);
+      });
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => id !== tour.id);
+    setTours(newTours);
+  };
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (isError) {
+    return (
+      <main>
+        <h2>There is an Error</h2>
+      </main>
+    );
+  }
+
+  if (tours.length < 1) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Reload
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({tour, removeTour}) => {
+  const {id, name, info, image, price} = tour 
+  const [articleInfo, setArticleInfo] = useState(`${info.slice(0,199)}...`)
+  const [open, setOpen] = useState(false)
+  const [buttonText, setButtonText] = useState("Read More")
+
+  const handleClick = e => {
+    if(open) {
+      setOpen(false)
+      setArticleInfo(`${info.slice(0,199)}...`)
+      setButtonText("Read More")
+    } else {
+      setButtonText("Show less")
+      setOpen(true)
+      setArticleInfo(info)
+    }
+  }
+
+  return <article className="single-tour">
+    <img src={image} alt={name}/>
+    <footer>
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <h4 className="tour-price">{price}</h4>
+      </div>
+      <p>{articleInfo}
+      <button type="button" onClick={handleClick}>{buttonText}</button></p>
+      <button className="delete-btn" onClick={() => removeTour(id)}>Not interested</button>
+    </footer>
+  </article>;
 };
 
 export default Tour;
