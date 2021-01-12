fbe17ca56e8ed3f5aade880b02cb301875961e5b
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-01-12 16:27:55 +0100

    finished menu project

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -4,7 +4,24 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menu, setMenu] = useState(items)
+
+  const filterMenu = (category) => {
+    let newMenu = []; 
+    category === "all" ? newMenu = items : newMenu = items.filter(item => item.category === category);
+    setMenu(newMenu)
+  }
+
+  return <main>
+    <section className="menu section">
+    <div className="title">
+      <h2>The Menu</h2>
+      <div className="underline"></div>
+    </div>
+      <Categories filterMenu={filterMenu} />
+      <Menu menu={menu} />
+    </section>
+  </main>;
 }
 
 export default App;
--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterMenu }) => {
+  return (
+    <div className="btn-container">
+      <button
+        type="button"
+        className="filter-btn"
+        onClick={() => filterMenu("all")}
+      >
+        All
+      </button>
+      <button
+        type="button"
+        className="filter-btn"
+        onClick={() => filterMenu("breakfast")}
+      >
+        Breakfast
+      </button>
+      <button
+        type="button"
+        className="filter-btn"
+        onClick={() => filterMenu("lunch")}
+      >
+        Lunch
+      </button>
+      <button
+        type="button"
+        className="filter-btn"
+        onClick={() => filterMenu("shakes")}
+      >
+        Shakes
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
