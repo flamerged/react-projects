ff3cf563ef136b35dbfa080a36d705c5292743e8
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-01-14 21:34:27 +0100

    saving progress on color generator project

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -4,7 +4,16 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  return <>
+  <section className="container">
+    <h3>Color Generator</h3>
+    <form >
+      <input type="text" placeholder="#ff206E" className="null"/>
+      <button type="submit" className="btn">Submit</button>
+    </form>
+  </section>
+  <section className="colors"></section>
+  </>
 }
 
 export default App
